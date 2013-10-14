e9bd30cf95fdebee8d21c51f10fe1401eba020fa
binhtv <binhtran.vnu@gmail.com>
2013-10-14 11:30:49 +0700

    laucher and home layout

---
+++ src/com/example/PilotProject/HomeActivity.java
@@ -0,0 +1,18 @@
+package com.example.PilotProject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * User: binhtv
+ * Date: 10/14/13
+ * Time: 10:47 AM
+ */
+public class HomeActivity extends Activity
+{
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
+}
---
+++ src/com/example/PilotProject/launchActivity.java
@@ -0,0 +1,18 @@
+package com.example.PilotProject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * User: binhtv
+ * Date: 10/14/13
+ * Time: 11:27 AM
+ */
+public class launchActivity extends Activity
+{
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.laucher);
+    }
+}
