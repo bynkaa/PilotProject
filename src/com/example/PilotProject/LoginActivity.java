e62a6718971b44adc993a2d38d73bcd6126d87fa
binhtv <binhtran.vnu@gmail.com>
2013-10-14 15:06:46 +0700

    laucher and home layout

--- src/com/example/PilotProject/LoginActivity.java
+++ src/com/example/PilotProject/LoginActivity.java
@@ -3,15 +3,16 @@ package com.example.PilotProject;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class MyActivity extends Activity
+/**
+ * User: binhtv
+ * Date: 10/14/13
+ * Time: 2:34 PM
+ */
+public class LoginActivity extends Activity
 {
-    /**
-     * Called when the activity is first created.
-     */
-    @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.login);
     }
 }
--- src/com/example/PilotProject/launchActivity.java
+++ src/com/example/PilotProject/launchActivity.java
@@ -1,18 +1,37 @@
 package com.example.PilotProject;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * User: binhtv
  * Date: 10/14/13
  * Time: 11:27 AM
  */
-public class launchActivity extends Activity
+public class LaunchActivity extends Activity
 {
+    Button btLoginFB;
+    Button btLogin;
+    Button btSignOut;
+
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.laucher);
+        btLogin = (Button) findViewById(R.id.btLogin);
+        btLogin.setOnClickListener(btLoginClickListener);
     }
+
+    private View.OnClickListener btLoginClickListener = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View view)
+        {
+            Intent intent = new Intent(LaunchActivity.this, LoginActivity.class);
+            startActivity(intent);
+        }
+    };
 }
