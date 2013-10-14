ba4d5e8a0b98f98d3c35c86ab0d938a7dae7bea7
thanhtranduc0706 <thanhtranit@gmail.com>
2013-10-13 19:43:52 -0700

    ảo vãi

--- gen/com/example/PilotProject/BuildConfig.java
+++ gen/com/example/PilotProject/BuildConfig.java
@@ -3,7 +3,6 @@
 /** Automatically generated file. DO NOT MODIFY */
 package com.example.PilotProject;
 
-public final class BuildConfig
-{
+public final class BuildConfig {
     public final static boolean DEBUG = true;
 }
---
+++ src/com/example/PilotProject/Login.java
@@ -0,0 +1,14 @@
+package com.example.PilotProject;
+
+import android.app.Activity;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: thanhtd
+ * Date: 10/14/13
+ * Time: 9:15 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Login extends Activity
+{
+}
