9487d6c32ae409f69fd12bf4ba200185c321d916
binhtv <binhtran.vnu@gmail.com>
2013-10-17 17:44:23 +0700

    aaaaa

--- src/com/qsoft/pilotproject/activity/ProgramFragment.java
+++ src/com/qsoft/pilotproject/activity/ProgramFragment.java
@@ -1,9 +1,13 @@
 package com.qsoft.pilotproject.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.view.View;
+import android.widget.ImageButton;
 import com.example.PilotProject.R;
+import com.qsoft.pilotproject.model.ProgramTab;
 
 /**
  * User: binhtv
@@ -12,9 +16,82 @@ import com.example.PilotProject.R;
  */
 public class ProgramFragment extends FragmentActivity
 {
+
+    FragmentManager manager;
+    ProgramTab currentTab = ProgramTab.THUMB_NAIL;
+    View.OnClickListener btCommentOnclickListner = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View view)
+        {
+            if (currentTab != ProgramTab.COMMENT)
+            {
+                currentTab = ProgramTab.COMMENT;
+                updateProgramFragment();
+            }
+        }
+
+    };
+    View.OnClickListener btDetailOnclickListener = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View view)
+        {
+            if (currentTab != ProgramTab.DETAIL)
+            {
+                currentTab = ProgramTab.DETAIL;
+                updateProgramFragment();
+            }
+        }
+    };
+    View.OnClickListener btThumbnailOnclickListener = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View view)
+        {
+            if (currentTab != ProgramTab.THUMB_NAIL)
+            {
+                currentTab = ProgramTab.THUMB_NAIL;
+                updateProgramFragment();
+            }
+        }
+    };
+    private ImageButton btThumbnail;
+    private ImageButton btDetail;
+    private ImageButton btComment;
+
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.program);
+        btThumbnail = (ImageButton) findViewById(R.id.ibThumbnail);
+        manager = getSupportFragmentManager();
+        btThumbnail.setOnClickListener(btThumbnailOnclickListener);
+        btDetail = (ImageButton) findViewById(R.id.ibDetail);
+        btDetail.setOnClickListener(btDetailOnclickListener);
+        btComment = (ImageButton) findViewById(R.id.ibComment);
+        btComment.setOnClickListener(btCommentOnclickListner);
+        updateProgramFragment();
     }
+
+    private void updateProgramFragment()
+    {
+        Fragment fragmentContainer = manager.findFragmentById(R.id.fragmentContainer);
+        switch (currentTab)
+        {
+            case DETAIL:
+                fragmentContainer = new DetailFragment();
+                break;
+            case COMMENT:
+                fragmentContainer = new CommentFragment();
+                break;
+            case THUMB_NAIL:
+                fragmentContainer = new ThumbnailFragment();
+                break;
+        }
+        manager.beginTransaction().replace(R.id.fragmentContainer, fragmentContainer).commit();
+
+    }
+
+
 }
