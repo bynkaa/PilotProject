2211355435d7009994f613e14a5f3b36a37c47dc
binhtv <binhtran.vnu@gmail.com>
2013-10-21 09:17:10 +0700

    big update

---
+++ src/com/qsoft/pilotproject/activity/ProgramFragment.java
@@ -0,0 +1,123 @@
+package com.qsoft.pilotproject.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.RadioGroup;
+import com.example.PilotProject.R;
+import com.qsoft.pilotproject.adapter.SideBarItemAdapter;
+import com.qsoft.pilotproject.model.ProgramTab;
+
+/**
+ * User: binhtv
+ * Date: 10/17/13
+ * Time: 8:59 AM
+ */
+public class ProgramFragment extends Fragment
+{
+
+    FragmentManager manager = getFragmentManager();
+    private ImageButton ibProgramBack;
+
+    ProgramTab currentTab = ProgramTab.THUMB_NAIL;
+    private RadioGroup rgProgramTab;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+    {
+        View view = inflater.inflate(R.layout.program,container,false);
+        rgProgramTab = (RadioGroup) view.findViewById(R.id.rgProgramTab);
+        rgProgramTab.setOnCheckedChangeListener(programTabOnCheckChangeListener);
+        rgProgramTab.check(R.id.rbThumbnail);
+        ibProgramBack = (ImageButton) view.findViewById(R.id.ibProgramBack);
+
+        ibProgramBack.setOnClickListener(ibProgramBackOnClickListener);
+        startContentPlayerFragment();
+        return view;
+    }
+
+
+    View.OnClickListener ibProgramBackOnClickListener = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View view)
+        {
+            Intent intent = new Intent(getActivity(), SlideBar.class);
+            startActivity(intent);
+        }
+    };
+    AdapterView.OnItemClickListener itemSideBarClickListner = new AdapterView.OnItemClickListener()
+    {
+
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
+        {
+            // on item click
+        }
+    };
+
+
+    RadioGroup.OnCheckedChangeListener programTabOnCheckChangeListener = new RadioGroup.OnCheckedChangeListener()
+    {
+        @Override
+        public void onCheckedChanged(RadioGroup radioGroup, int i)
+        {
+            int checkedRbTab = rgProgramTab.getCheckedRadioButtonId();
+            switch (checkedRbTab)
+            {
+                case R.id.rbThumbnail:
+                    currentTab = ProgramTab.THUMB_NAIL;
+                    break;
+                case R.id.rbDetail:
+                    currentTab = ProgramTab.DETAIL;
+                    break;
+                case R.id.rbComment:
+                    currentTab = ProgramTab.COMMENT;
+                    break;
+            }
+            updateProgramFragment();
+        }
+    };
+
+    private void startContentPlayerFragment()
+    {
+        Fragment playerFragment = getFragmentManager().findFragmentById(R.id.contentPlayerFragment);
+        if (playerFragment == null)
+        {
+            playerFragment = new ContentPlayerFragment();
+            getFragmentManager().beginTransaction().add(R.id.contentPlayerFragment, playerFragment).commit();
+        }
+
+    }
+
+    private void updateProgramFragment()
+    {
+        Fragment fragmentContainer = getFragmentManager().findFragmentById(R.id.fragmentContainer);
+        switch (currentTab)
+        {
+            case DETAIL:
+                fragmentContainer = new DetailFragment();
+                break;
+            case COMMENT:
+                fragmentContainer = new CommentFragment();
+                break;
+            case THUMB_NAIL:
+                fragmentContainer = new ThumbnailFragment();
+                break;
+        }
+
+        getFragmentManager().beginTransaction().replace(R.id.fragmentContainer, fragmentContainer).commit();
+
+    }
+
+
+}
---
+++ src/com/qsoft/pilotproject/activity/SlideBar.java
@@ -0,0 +1,88 @@
+package com.qsoft.pilotproject.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.widget.DrawerLayout;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import com.example.PilotProject.R;
+import com.qsoft.pilotproject.adapter.SideBarItemAdapter;
+
+/**
+ * User: binhtv
+ * Date: 10/14/13
+ * Time: 10:47 AM
+ */
+public class SlideBar extends FragmentActivity
+{
+    public static final String[] SIDE_BAR_ITEMS = new String[]{"HomeFragment", "Favorite", "Following", "Audience",
+            "Genres", "Setting", "Help Center", "Sign Out"};
+    public static final Integer[] SIDE_BAR_ICONS = new Integer[]{
+            R.drawable.sidebar_imageicon_home,
+            R.drawable.sidebar_image_icon_favorite,
+            R.drawable.sidebar_image_icon_following,
+            R.drawable.sidebar_image_icon_audience,
+            R.drawable.sidebar_image_icon_genres,
+            R.drawable.sidebar_image_icon_setting,
+            R.drawable.sidebar_image_icon_helpcenter,
+            R.drawable.sidebar_image_icon_logout
+    };
+
+
+    private ListView lvSlideBar;
+    private View leftDrawerView;
+    private DrawerLayout dlSlideBar;
+    private ActionBarDrawerToggle actionBarDrawerToggle;
+
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.slidebar);
+
+        dlSlideBar = (DrawerLayout) findViewById(R.id.drawer_layout);
+        lvSlideBar = (ListView) findViewById(R.id.lvSlideBar);
+        leftDrawerView = findViewById(R.id.left_drawer_home);
+        setListViewSlideBar();
+        lvSlideBar.setOnItemClickListener(itemSideBarClickListner);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                dlSlideBar,         /* DrawerLayout object */
+                R.drawable.ic_launcher,  /* nav drawer icon to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description */
+                R.string.drawer_close  /* "close drawer" description */);
+
+        Fragment homeFragment = new HomeFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.content_fragment, homeFragment).commit();
+        dlSlideBar.setDrawerListener(actionBarDrawerToggle);
+    }
+
+
+    AdapterView.OnItemClickListener itemSideBarClickListner = new AdapterView.OnItemClickListener()
+    {
+
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
+        {
+            // on item click
+        }
+    };
+
+    public void setListViewSlideBar()
+    {
+        SideBarItemAdapter sideBarItemAdapter = new SideBarItemAdapter(this, R.layout.menu, SIDE_BAR_ITEMS);
+        lvSlideBar.setAdapter(sideBarItemAdapter);
+    }
+
+    public void setOpenOption()
+    {
+        dlSlideBar.openDrawer(leftDrawerView);
+
+    }
+    public void setCloseOption()
+    {
+        dlSlideBar.closeDrawer(leftDrawerView);
+    }
+}
